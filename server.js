905f863f0c783d53b9edf1fcc316e172363808d8
Shehbaz33 <shehbazsayed33@gmail.com>
2022-01-04 16:59:51 +0530

    added procfile

--- server.js
+++ server.js
@@ -60,6 +60,10 @@ app.use(errorMiddlerware);
 // DEPLOYING TO PRODUCTION ON HEROKU
 if(process.env.NODE_ENV == 'production'){
   app.use(express.static("client/build"));
+  const path = require('path');
+  app.get("*",(req,res) => {
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+  })
 }
 
 // STARTING SERVER
