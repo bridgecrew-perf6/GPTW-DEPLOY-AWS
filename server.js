d8b33a4ffa180add02653ee94c0a7d8020c35300
Shehbaz33 <shehbazsayed33@gmail.com>
2022-01-04 17:07:33 +0530

    changed for deployement

--- server.js
+++ server.js
@@ -59,10 +59,10 @@ app.use(errorMiddlerware);
 
 // DEPLOYING TO PRODUCTION ON HEROKU
 if(process.env.NODE_ENV == 'production'){
-  app.use(express.static("client/build"));
   const path = require('path');
+  app.use(express.static(path.resolve(__dirname,"./client/build")));
   app.get("*",(req,res) => {
-    res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    res.sendFile(path.resolve(__dirname,'./client/build','index.html'));
   })
 }
 
